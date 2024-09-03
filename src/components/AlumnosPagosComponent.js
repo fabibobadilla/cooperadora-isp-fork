7e27afd50c15c779f5fb353abf3685633c28f6db
Agustín Alesso <agustinalesso23@gmail.com>
2024-09-03 18:24:18 -0300

    Edicion del pago finalizada

--- src/components/AlumnosPagosComponent.js
+++ src/components/AlumnosPagosComponent.js
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './AlumnosPagosComponent.module.css';
 import { formatDate, formatNumberToCurrency } from '@/utils/format-helpers';
+import Swal from 'sweetalert2';
 
 export default function AlumnosPagosComponent({ pagos }) {
+
+  const [pagosState, setPagosState] = useState(pagos);
+
+  const efectuarPago = (pagoId) => {
+    Swal.fire({
+      title: "¿Estás seguro/a?",
+      text: `Estas por abonar el pago. ¿Desea continuar?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Confirmar pago",
+      cancelButtonText: 'Cancelar'
+    }).then(result => {
+      if(result.isConfirmed){
+        fetch('http://localhost:3000/api/pagos', {
+          method: 'PUT',
+          body: JSON.stringify({
+            id: pagoId
+          })
+        }).then(respuesta => respuesta.json())
+          .then(data => {
+            const pagosModificados = pagosState.map(pago => {
+              if(pago._id === data._id){
+                pago.pagado = true;
+              }
+
+              return pago;
+            });
+            setPagosState(pagosModificados);
+          })
+          .catch(error => console.error(error));
+      }
+    });
+  }
+
   return <div className={styles.detallePagos}>
     <h2 className={styles.tituloPagos}>Pagos</h2>
     <ul className={styles.pagoList}>
       {
-        pagos.map(pago => <li key={pago._id}>
+        pagosState.map(pago => <li key={pago._id}>
           <div className={styles.pagoItem}>
             <span className={pago.pagado ? styles.abonado : styles.pendiente}>
-              {pago.pagado ? 'Abonado' : 'Pendiente'}
+              { pago.pagado ? 'Abonado' : 'Pendiente' }
             </span>
             <span className={styles.fechaPago}>{formatDate(pago.fechaCreacion)}</span>
             <div className={styles.pagoItemDetalle}>
               <span className={styles.conceptoPago}>{pago.cobro_id.titulo}</span>
               <span className={styles.montoPago}>{formatNumberToCurrency(pago.cobro_id.monto)}</span>
-              <button disabled={pago.pagado} className={styles.abonarBtn}>Abonar</button>
+              <button disabled={pago.pagado} onClick={() => efectuarPago(pago._id)} className={styles.abonarBtn}>Abonar</button>
             </div>
           </div>
         </li>)
--- src/pages/api/pagos.js
+++ src/pages/api/pagos.js
@@ -18,4 +18,22 @@ export default async function handler(req, res) {
       res.status(500).send(error);
     }
   }
+
+  if(method === 'PUT') {
+    try{
+      const data = JSON.parse(req.body);
+      // Fetch a backend con metodo PUT
+      const response = await fetch('http://localhost:1973/pagos', {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': "application/json"
+        }
+      })
+      const responseData = await response.json();
+      res.send(responseData);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
 }
