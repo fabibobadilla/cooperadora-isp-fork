076bbe796db74e4a96889c19a9b6f5c10aaf1514
FBWinster22 <fbwinster22@gmail.com>
2024-10-15 18:19:11 -0300

    actualizacion

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
+'use client'
+
 import "./globals.css";
 import RootMenu from '@/components/RootMenu';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const metadata = {
   title: "Cooperadora ISP 20 San Justo | Sistema de Gestión",
@@ -7,12 +11,27 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+  const pathname = usePathname();
+
   return (
     <html lang="es">
       <body>
         <RootMenu />
         <div className='layout-wrapper'>
-          {children}
+          <div className="menu-nav" >
+            <nav className="nav-main">
+              <ul>
+                <Link href='/usuarios'><li className={pathname === '/usuarios' ? style.active : ''}>Usuarios</li></Link>
+                <Link href='/contacto'><li className={pathname === '/contacto' ? style.active : ''}>Contacto</li></Link>
+                <Link href='/paginaoficial'><li className={pathname === '/paginaoficial' ? style.active : ''}>Sitio oficial</li></Link>
+                <Link href='/informacion'><li className={pathname === '/informacion' ? style.active : ''}>Información</li></Link>
+              </ul>
+            </nav>
+          </div>
+          <div>
+          {children}  
+          </div>
         </div>
       </body>
     </html>
--- src/components/RootMenu.js
+++ src/components/RootMenu.js
@@ -1,13 +1,18 @@
+'use client' 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function RootMenu(){
+
+const pathname = usePathname();
+
   return <div className='layout-menu'>
           <h1 className='menu-title'>Cooperadora ISP 20</h1>
           <ul>
-            <Link href="/"><li>Inicio</li></Link>
-            <Link href="/alumnos"><li>Alumnos</li></Link>
-            <Link href="/cobros"><li>Cobros</li></Link>
-            <li>Reportes</li>
+            <Link legacyBehavior href="/"><li className={pathname === '/' ? 'active' : ''}>Inicio</li></Link>
+            <Link legacyBehavior href="/alumnos"><li className={pathname === '/alumnos' ? 'active' : ''}>Alumnos</li></Link>
+            <Link legacyBehavior href="/cobros"><li className={pathname === '/cobros' ? 'active' : ''}>Cobros</li></Link>
+            <Link legacyBehavior href='/reportes'><li className={pathname === '/reportes' ? 'active' : ''}>Reportes</li></Link>
           </ul>
         </div>
 }
